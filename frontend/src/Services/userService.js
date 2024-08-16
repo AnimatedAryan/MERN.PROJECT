e30799de92005b66f7f261eafbee9201ba69c8fc
Aryan Birkhani <aryanbirkhani214@gmail.com>
2024-08-17 02:35:45 +0530

    new commits made

--- frontend/src/Services/userService.js
+++ frontend/src/Services/userService.js
@@ -1,8 +1,9 @@
 import axios from 'axios';
 
 export const fetchUserProfile = async () => {
+    const backend=import.meta.env.vite_backend_url;
     try {
-        const response = await axios.get('http://localhost:8000/api/user/profile', {
+        const response = await axios.get(`${backend}/api/user/profile`, {
             withCredentials: true 
         });
         return response.data.user;
